fd83a9605a9992d14e2842a62dc41aec426cd06c
CrusaderCoder73 <mbustam9@msudenver.edu>
2024-02-21 15:31:53 -0700

    Added Project File
    Added project file that contains the template for the project

---
+++ project2.py
@@ -0,0 +1,33 @@
+#Matthew
+def copyFile():
+    """copies the original content of the input file to
+    the output file."""
+    pass
+
+#Matthew
+def functionFormater():
+    """check for the def keyword and check if the function 
+    header is correct if not fix it"""
+    pass
+
+# Haimei
+def indentFormatter():
+    """check if a colon is being used if so check he indent
+    and if its wrong fix it"""
+    pass
+
+# Christina
+def printCounter():
+    """Count the number of time the print keyword is used"""
+    pass
+# Haimei
+def outputFile():
+    """Output The New File to the output file"""
+    pass
+
+def main():
+    """Main Function"""
+    pass
+
+if __name__ == "__main__":
+    main()
